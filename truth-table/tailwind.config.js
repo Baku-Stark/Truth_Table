037f87204b8ce29a5ba2de09756ca4e83dc31db7
Izabel <limaisabel037@gmail.com>
2025-07-11 21:04:13 -0300

    fix: use Tailwind v3.4.3 because Vue CLI does not work with v4

--- truth-table/tailwind.config.js
+++ truth-table/tailwind.config.js
@@ -5,17 +5,5 @@ export default {
   content: [
     './index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'feminine-pink': '#ffb6c1',
-        'feminine-lavender': '#d8bfd8',
-        'feminine-rose': '#f9c6d0',
-        'feminine-dark': '#1e1e2f',
-        'feminine-accent': '#ff69b4',
-      },
-    },
-  },
-  plugins: [],
+  ]
 }
